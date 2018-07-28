f10bacf673712ac025d188071029806d08e1b070
Jordan Crawford <jordan@crawford.kiwi>
2018-07-28 22:36:50 +1200

    Fix a bug where identical transactions on the same day would result not result in additional transactions.
    The `import_id` now takes occurences into account to prevent this issue by counting the number of times the row hash has been seen and including this occurence count in computing the actual hash.
    This works similar to the rerun index which results in new hashes in order to support intentional re-importing.

--- import.js
+++ import.js
@@ -8,17 +8,16 @@ let hash = require('object-hash');
 let args = process.argv.slice(2)
 
 if (args.length < 4 || args.length > 6) {
-    console.log("node import.js [GoodBudget input file] [YNAB Access Token] [YNAB budget ID] [YNAB account ID] [optional - date format, default: MM/DD/YYYY] [optional - rerun index, default: 0")
+    console.log("node import.js [GoodBudget input file] [YNAB Access Token] [YNAB budget ID] [YNAB account ID] [optional - date format, default: MM/DD/YYYY] [optional - rerun index, default: 0]")
     return;
 }
 
 runImport(...args)
 
-let rowIndex = 0;
 async function runImport(inputFileName, ynabAccessToken, ynabBudgetId, ynabAccountId, dateFormat = "MM/DD/YYYY", rerunIndex = 0) {
     let stream = fs.createReadStream(inputFileName);
     let ynabAPI = new ynab.API(ynabAccessToken);
-    
+
     try {
         let categoryGroups = await ynabAPI.categories.getCategories(ynabBudgetId);
         let categoryIds = categoryGroups.data.category_groups.reduce(function(accum, group) {
@@ -26,6 +25,7 @@ async function runImport(inputFileName, ynabAccessToken, ynabBudgetId, ynabAccou
             return accum;
         }, {});
         
+        let transactionHashes = {};
         stream
             .pipe(csv({headers: true}))
             .transform(function (row) {
@@ -35,7 +35,10 @@ async function runImport(inputFileName, ynabAccessToken, ynabBudgetId, ynabAccou
                 let date = moment(row.Date, dateFormat).toISOString();
                 let truncatedPayeeName = row.Name.length == 0 ? null : row.Name.substring(0, 50);
                 let amount = Math.round(parseFloat(row.Amount) * 1000);
-                let importId = "GBI:" + hash.MD5(Object.assign({ index: rowIndex, rerun: rerunIndex }, row));
+
+                let transactionHash = hash.MD5(row);
+                let occurenceIndex = transactionHashes[transactionHash] = (transactionHashes[transactionHash] || 0) + 1
+                let importId = "GBI:" + hash.MD5(Object.assign({ occurence: occurenceIndex, rerun: rerunIndex }, row));
 
                 let memo = row.Notes + row.Details;
                 if (!categoryId && row.Envelope.length > 0) {
@@ -54,8 +57,6 @@ async function runImport(inputFileName, ynabAccessToken, ynabBudgetId, ynabAccou
                     cleared: 'cleared',
                     import_id: importId
                 };
-
-                rowIndex++;
 
                 return data;
             })
